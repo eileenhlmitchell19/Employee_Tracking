842ece5c9c74d4ac31011e93cce621d7e53365da
Eileen Mitchell <eileenhlmitchell19@gmail.com>
2022-02-20 15:08:36 -0800

    updates including switch statement that did not work

--- index.js
+++ index.js
@@ -25,38 +25,32 @@ db.connect(function (err) {
 
 //--------------------- End Connect to database-----------------------------//
 
-
 //--------------------- VIEW ALL DEPARTMENTS -----------------------------//
 async function viewAllDepartments() {
-   db.query("SELECT * FROM department", (err, results) => {
-    if(err) throw err;
-    console.log('------------DEPARTMENT TABLE------------')
-       console.table(results);
-   });
-
+  db.query("SELECT * FROM department", (err, results) => {
+    if (err) throw err;
+    console.log("------------DEPARTMENT TABLE------------");
+    console.table(results);
+  });
 }
-
 
 //--------------------- VIEW ALL ROLES  -----------------------------//
 // view all roles - READ - SELECT * FROM [table_name]
 async function viewAllRoles() {
   db.query("SELECT * FROM role", (err, results) => {
-    if(err) throw err;
-    console.log('------------ROLE TABLE------------')
-      console.table(results);
+    if (err) throw err;
+    console.log("------------ROLE TABLE------------");
+    console.table(results);
   });
-
 }
-
 
 //--------------------- VIEW ALL EMPLOYEES -----------------------------//
 //view all employees - READ - SELECT * FROM [table_name]
 async function viewAllEmployees() {
-  db.query("SELECT * FROM employee",(err, results) => {
-    if(err) throw err;
+  db.query("SELECT * FROM employee", (err, results) => {
+    if (err) throw err;
     console.table(results);
   });
-    
 }
 ///FINISH
 
@@ -75,12 +69,44 @@ function start() {
           "View all Departments",
           "Create Role",
           "View all Roles",
-          "Quit"
+          "Quit",
         ],
-      }
+      },
     ])
     .then((response) => {
       console.log(response);
+      // let choices = response.options;
+      // switch (choices) {
+      //   case 'Create an Employee':
+      //     createEmployee();
+      //       break;
+
+      //   case 'View all Employees':
+      //     viewAllEmployees();
+      //       break;
+
+      //   case 'Create Department':
+      //     createDepartment();
+      //       break;
+
+      //   case 'View all Departments':
+      //     viewAllDepartments();
+      //       break;
+
+      //   case 'Create Role':
+      //       createRole();
+      //       break;
+
+      //   case 'View all Roles':
+      //       viewAllRoles();
+      //       break;
+
+
+//-----quit is not a function yet
+        // case 'Quit':
+        //     Quit();
+        //     break;
+    // }
       if (response.answers === "Create Role") {
         createRole();
       } else if (response.answers === "View all Roles") {
@@ -93,117 +119,118 @@ function start() {
         viewAllDepartments();
       } else if (response.answers === "Create Department") {
         createDepartment();
-      } else (response.answers === "Quit") 
+      } else response.answers === "Quit";
     });
 }
 //-------------------------------------------------------------------//
-
-
-
 
 //--------------------- CREATE DEPARTMENT -----------------------------//
 function createDepartment() {
   inquirer
     .prompt([
       {
-          type:'Input',
-          name: 'departmentName',
-          message:'What is the department name?'
-      }
+        type: "Input",
+        name: "departmentName",
+        message: "What is the department name?",
+      },
     ])
     // .then(answers => {
     //   console.log(answers);
-    .then(answers => { 
-        teamMembers.push( new Department ( answers.departmentName));
-        // arrayID.push(manager.id)
-        console.log(answers)
-        console.log(teamMembers)
-        // buildTeam();
-        createDepartment();
-        // start();
-    })
-//-------------------------------------------------------------------//
-
-//--------------------- CREATE EMPLOYEE -----------------------------//
-function createEmployee() {
-  inquirer
-    .prompt([
-      {
-          type:'Input',
-          name: 'firstName',
-          message:'What is the employees first name?'
-      },
-      {
-          type:'Input',
-          name: 'lastName',
-          message:'What is the employees last name?'
-      },
-      {
-          type:'Input',
-          name: 'lastName',
-          message:'What is the role id of the employee (Engineering = 1, Planning = 2)?'
-      }
-    ])
-    // .then(answers => {
-    //   console.log(answers);
-    .then(answers => { 
-        teamMembers.push( new Employee ( answers.employeeName));
-        // arrayID.push(manager.id)
-        console.log(answers)
-        console.log(teamMembers)
-        // buildTeam();
-        createEmployee();
-        // start();
-    })
-    
-//-------------------------------------------------------------------//
-
-//--------------------- CREATE ROLE -----------------------------//
-function createRole() {
-  inquirer
-    .prompt([
-      {
-          type:'Input',
-          name: 'employeeName',
-          message:'What is the employee name?'
-      },
-      {
-        type: "Input",
-        name: "roleSalary",
-        message: "What is the role salary?",
-      },
-      {
-        type: "Input",
-        name: "roleTitle",
-        message: "What is the role title?",
-      },
-      {
-        type: "Input",
-        name: "departmentId",
-        message: "What is the department id?",
-      },
-    ])
     .then((answers) => {
+      teamMembers.push(new Department(answers.departmentName));
+      // arrayID.push(manager.id)
       console.log(answers);
+      console.log(teamMembers);
+      // buildTeam();
+      createDepartment();
+      // start();
+    });
+  //-------------------------------------------------------------------//
 
-      let query = db.query(
-        "INSERT INTO role SET ?",
+  //--------------------- CREATE EMPLOYEE -----------------------------//
+  function createEmployee() { 
+    console.log("in")
+    inquirer
+      .prompt([
         {
+          type: "Input",
+          name: "firstName",
+          message: "What is the employees first name?",
+        },
+        {
+          type: "Input",
+          name: "lastName",
+          message: "What is the employees last name?",
+        },
+        {
+          type: "Input",
+          name: "lastName",
+          message:
+            "What is the role id of the employee (Engineering = 1, Planning = 2)?",
+        },
+      ])
+      console.log("in2")
+      // .then(answers => {
+      //   console.log(answers);
+      .then((answers) => {
+        console.log("in3")
+        teamMembers.push(new Employee(answers.firstName));
+        // arrayID.push(manager.id)
+        console.log(answers);
+        console.log(teamMembers);
+        // buildTeam();
+        // createEmployee();
+        // start();
+      });
+  }
+  //-------------------------------------------------------------------//
+
+  //--------------------- CREATE ROLE -----------------------------//
+  function createRole() {
+    inquirer
+      .prompt([
+        {
+          type: "Input",
+          name: "employeeName",
+          message: "What is the employee name?",
+        },
+        {
+          type: "Input",
+          name: "roleSalary",
+          message: "What is the role salary?",
+        },
+        {
+          type: "Input",
+          name: "roleTitle",
+          message: "What is the role title?",
+        },
+        {
+          type: "Input",
+          name: "departmentId",
+          message: "What is the department id?",
+        },
+      ])
+      .then((answers) => {
+        console.log(answers);
+
+        let query = db.query(
+          "INSERT INTO role SET ?",
+          {
             title: answers.roleTitle,
             salary: answers.roleSalary,
             department_id: answers.departmentId,
-        },
-        function (res, err) {
-          if (err) throw err;
-          console.log(res);
-        }
-      );
+          },
+          function (res, err) {
+            if (err) throw err;
+            console.log(res);
+          }
+        );
 
-      console.log("query", query);
-    })
-    .then(() => {
-      start();
-    });
+        console.log("query", query);
+      })
+      .then(() => {
+        start();
+      });
+  }
+  //-------------------------------------------------------------------//
 }
-//-------------------------------------------------------------------//
-}};
